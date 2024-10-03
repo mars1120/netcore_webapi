5d7eb10222c1ef942d0099a5bd6ba0f027f23a28
marschen <peach414@gmail.com>
2024-10-03 12:19:10 +0800

    docker compose over HTTPS
    https://learn.microsoft.com/zh-tw/aspnet/core/security/docker-compose-https?view=aspnetcore-8.0

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,10 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 builder.Services.AddControllers();
 
@@ -9,16 +15,37 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// catch logger
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+// add logs
+logger.LogInformation("Application is starting up.");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    logger.LogInformation("Swagger has been enabled for the development environment.");
 }
 
 app.UseHttpsRedirection();
+logger.LogInformation("HTTPS redirection has been configured.");
 
 app.UseAuthorization();
+logger.LogInformation("Authorization middleware has been added.");
+
 app.MapControllers();
+logger.LogInformation("Controllers have been mapped.");
+
+// // test
+// app.MapGet("/test", () => {
+//     logger.LogInformation("Test endpoint was called.");
+//     return "Hello from the test endpoint!";
+// });
+
+logger.LogInformation("Application is about to start running.");
 
 app.Run();
+
+logger.LogInformation("Application has stopped.");
